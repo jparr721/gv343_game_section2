efa62660117e0048ffcc9d70ae37453e6a8e4ea6
Jarred Parr <jparr721@gmail.com>
2018-10-01 11:14:50 -0400

    scaffold initial code

---
+++ Intro.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "Intro.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+
+namespace intro {
+  int Intro::display() {
+    sf::Texture spash;
+
+    if (!spash.loadFromFile("./images/neighborhood.png")) {
+      return EXIT_FAILURE;
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(spash);
+
+    sf::Font font;
+    if (!font.loadFromFile("./fonts/COMIC.ttf")) {
+      return EXIT_FAILURE;
+    }
+
+    sf::Text title;
+    title.setFont(font);
+    title.setString("OH BOY OH BOY OH BOY");
+    title.setCharacterSize(84);
+    title.setFillColor(sf::Color::Red);
+    title.setPosition(10, 200);
+
+    sf::Text text;
+    text.setFont(font);
+    text.setString("PrEsS EnTeR tO C0nT1Nu3");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(150, 350);
+
+    sf::Clock clock;
+
+    sf::SoundBuffer sound_buffer;
+    if (!sound_buffer.loadFromFile("music/epic_hero.wav")) {
+      text.setFillColor(sf::Color::Black);
+    } else {
+      text.setFillColor(sf::Color::White);
+    }
+  }
+} // namespace intro
---
+++ include/Intro.hpp
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_INTRO_HPP
+#define INCLUDE_INTRO_HPP
+
+namespace intro {
+  class Intro {
+    public:
+      Intro() = default;
+      ~Intro() = default;
+
+      int display();
+  };
+} // namespace intro
+
+#endif
