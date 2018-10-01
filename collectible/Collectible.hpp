70ae2697dfeed23f547b42e2fbe2055a765bdc00
Nick Beukema <nick.beukema@gmail.com>
2018-10-01 11:54:26 -0400

    Add base collectible header file

---
+++ collectible/Collectible.hpp
@@ -0,0 +1,17 @@
+#ifndef		__H__COLLECTIBLE
+#define		__H__COLLECTIBLE
+
+// #include "SFML/Graphics.hpp"
+// #include <string>
+
+class Collectible {
+	private:
+		sf::Texture texture;
+		sf::Sprite sprite;
+
+	public:
+		Collectible();
+		sf::Sprite getSprite();
+};
+
+#endif
