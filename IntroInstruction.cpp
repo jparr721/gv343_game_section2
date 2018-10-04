bbb2340b21b31c2af3886a993311d7826c74c9e1
David Baas <David@Rogers-MacBook-Pro.local>
2018-10-04 16:59:21 -0400

    Wrote IntroInstruction class

---
+++ IntroInstruction.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include "SFML/System.hpp"
+#include "IntroInstruction.hpp"
+
+namespace intro{
+  namespace moving_scene{
+    IntroInstruction::IntroInstruction(
+        int entityID, 
+        std::string action, 
+        std::string detail){
+
+      this->entityID = entityID;
+      this->detail = detail;
+      IntroInstruction::initAction(action);
+    }
+
+    std::string IntroInstruction::getText() const {
+      return detail;
+    }
+
+    sf::Vector2f IntroInstruction::getVector() const{
+      std::string values[2];
+      char tokenLoc = detail.find(",");
+
+      values[0] = detail.substr(0,tokenLoc);
+      values[1] = detail.substr(tokenLoc + 1, detail.length());
+      return sf::Vector2f(std::stof(values[0]), std::stof(values[1]));
+    }
+
+    void IntroInstruction::initAction(std::string action){
+      if(action == "MOVE_ENTITY"){
+        this->action = MOVE_ENTITY;
+      }
+      else if(action == "SET_POSITION"){
+        this->action = SET_POSITION;
+      }
+      else if(action == "TOGGLE__ENTITY"){
+        this->action = TOGGLE__ENTITY;
+      }
+      else if(action == "DISPLAY_SPEECH"){
+        this->action = DISPLAY_SPEECH;
+      }
+      else if(action == "PLAY_SOUND"){
+        this->action = PLAY_SOUND;
+      }
+      else if(action == "WAIT"){
+        this->action = WAIT;
+      }
+      else{
+        //throw error
+      }
+    }
+  }
+}
+
--- include/IntroInstruction.hpp
+++ include/IntroInstruction.hpp
@@ -7,31 +7,79 @@
 
 namespace intro{
   namespace moving_scene{
+    /******************************************************************
+     * The IntroInstruction class holds the instruction for a 
+     * sequenced scene in memory. It hold information such as what 
+     * entity is being modified, the action being taken, and details
+     * related to the action. It is inteded that a series of action
+     * is made ahead of time and saved in a .csv file. From there
+     * the MovingScene class will load it into memeory.
+     *
+     * Author David Baas
+     * Version 1.0 - 10/4/2018
+     * Since 1.0 - 10/4/2018
+     *****************************************************************/
     class IntroInstruction{
-      public: 
-        IntroInstruction(int entityID, std::string text, char action,
-            int deltaX, int deltaY);
-
+      public:
+        /**************************************************************
+         * The constructor initalizes all of the varable. 
+         *
+         * Param: entityID the id of the entity being modified by this
+         * instruction.
+         * Param: action the action being taken with this instruction.
+         * Param: The details of this instruction.
+         *************************************************************/
+        IntroInstruction(int entityID, std::string action, std::string detail);
+            
+        /**************************************************************
+         * Return the details of this instruction as a string. Used 
+         * for getting file names and speech. 
+         *
+         * Return: The value of detail as a string.
+         *************************************************************/
         std::string getText() const;
 
+        /**************************************************************
+         * Returns the details of the instruction as a vector. Used
+         * for getting a position or a movement.
+         *
+         * Return: The value of details as a vector.
+         *************************************************************/
         sf::Vector2f getVector() const;
 
+        /*A series of enumerated types to define actions*/
         enum Action :unsigned char {
           MOVE_ENTITY,
           SET_POSITION,
           TOGGLE__ENTITY,
           DISPLAY_SPEECH, 
-          PLAY_SOUND
+          PLAY_SOUND,
+          WAIT
         };
 
       private:
+        /**************************************************************
+         * All the entities will be held in a vector.
+         * This value tracks what entity is being modified by this
+         * instruction. If an action is not being done on an entitiy 
+         * the value should be -1.
+         *************************************************************/
         int entityID;
+
+        /*The action being taken with this instruction.*/
         Action action;
+
+        /**************************************************************
+         * The detials of the instruction weather its text on the screen,
+         * a vector value, a lentgh of time to wait, or the file to
+         * play for a sound file.
+         **************************************************************/
         std::string detail;
+
+        /*A private method to initialize the action.*/
+        void initAction(std::string action);
     };
   }
 }
-
-
-
 #endif
+
