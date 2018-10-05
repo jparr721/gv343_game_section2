a1bdac0dafdda1a75bafd94d18fe690a3f0aba8c
Johnathon Killeen <JohnSK333@gmail.com>
2018-10-05 11:26:00 -0400

    Updates to Weapon
    Trying to create an attack with a delay

--- Weapons.cpp
+++ Weapons.cpp
@@ -7,19 +7,47 @@ void Weapons::initalize(){
     range = 0;
     damage = 0;
     equipped = false;
+    clock = 0;
+}
+
+void Weapons::setWeapon(int range, int damage, sf::Sprite sprite,bool equipped){
+    this->range = range;
+    this->damage = damage;
+    this->sprite = sprite;
+    this->equipped = equipped;
 }
 
 void Weapons::setRange(int range){
     this->range = range;
 }
 
+int Weapons::getRange(){
+    return range;
+}
+
 void Weapons::setDamage(int damage){
     this->damage = damage;
 }
 
+int Weapons::getDamage(){
+    return damage;
+}
 void Weapons::setPosition(int x, int y){
     this->x = x;
     this->y = y;
+}
+
+void attack(){
+    sf::Time time = clock.getElapsedTime();
+    sf::Int32 mills = time.asMilliseconds();
+
+    if(mills % 1000 > 500){
+        int range = Weapons::getRange();
+        int damage = Weapons::getDamage();
+        //attack these dudes
+        //I'm trying to make a way to put a delay between attacks
+        //god help me
+    }
 }
 
 sf::Sprite Weapons::getSprite(){
--- Weapons.hpp
+++ Weapons.hpp
@@ -8,14 +8,20 @@ class Weapons {
     public:
         Weapons();
         void initalize();
+        void setWeapon(int range, int damage, sf::Sprite sprite, bool equipped);
         void setRange(int range);
         void setDamage(int damage);
         void setPosition(int x, int y);
+        void attack();
+        int getRange();
+        int getDamage();
         sf::Sprite getSprite();
+
 
     private:
         sf::Sprite sprite;
         sf::Texture texture;
+        sf:Clock clock;
         int range;
         int damage;
         int x;
