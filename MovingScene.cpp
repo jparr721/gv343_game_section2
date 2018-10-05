c0ff47dd3d51e4972d2ecb3183958fefedd18f7c
David Baas <David@Rogers-MacBook-Pro.local>
2018-10-05 01:29:50 -0400

    Implemented initEntities

---
+++ MovingScene.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "IntroEntity.hpp"
+#include "IntroInstruction.hpp"
+#include "MovingScene.hpp"
+
+namespace intro{
+  namespace moving_scene{
+    MovingScene::MovingScene(
+        std::string entitiesFilename,
+        std::string instructionsFilename,
+        std::string soundBufferFilename,
+        std::string music,
+        sf::Music &player){
+
+    }
+
+    void  MovingScene::initEntities(std::string &entitiesFilename){
+      constexpr int BUFFER_SIZE = 5; 
+
+      std::ifstream entityList(entitiesFilename);
+      if(!entityList){
+        //Throw IO error 
+      }
+
+      //Define a buffer and get the first line containing the
+      //number of entities
+      std::string buffer[BUFFER_SIZE];
+      std::getline(entityList, buffer[0]);
+      size_t size = std::stoi(buffer[0]);
+
+      //test something 
+      for(int index = 0; index < size; ++index){
+        //Split the line
+        for(int buffer_index = 0; buffer_index < BUFFER_SIZE; 
+            ++buffer_index){
+          std::getline(entityList, buffer[buffer_index], ',');
+        }
+
+        //Extract the scale and position of the entity.
+        Scale scale(std::stof(buffer[1]), std::stof(buffer[2]));
+        Position position(std::stof(buffer[3]), std::stof(buffer[4]));
+
+        entities.push_back(IntroEntity(buffer[0],scale, position));
+
+      }
+
+      entityList.close();
+    }
+
+    void MovingSceneinitInstructions(std::string &instructionsFilename){
+
+    }
+
+    void MovingScene::initSounds(std::string &soundBufferList){
+
+    }
+
+  }
+}
--- include/MovingScene.hpp
+++ include/MovingScene.hpp
@@ -1,9 +1,11 @@
 #ifndef         MOVING_SCENE_HPP
 #define         MOVING_SCENE_HPP
 
+#include <string>
 #include <vector>
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
+#include "IntroEntity.hpp"
 #include "IntroInstruction.hpp"
 
 
@@ -11,16 +13,40 @@ namespace intro{
   namespace moving_scene{
     class MovingScene{
       public:
-        MovingScene();
+        MovingScene(
+            std::string entitiesFilename, 
+            std::string instructionsFilename,
+            std::string soundBufferFilename,
+            std::string music,
+            sf::Music &player);
+
+        void run(sf::RenderWindow &window);
 
       private:
-        std::vector<sf::Sprite> entities;
+        std::vector<IntroEntity> entities;
         std::vector<IntroInstruction> instructions;
         std::vector<sf::SoundBuffer> bufferedFiles;
         std::vector<sf::Sound> sounds;
         sf::Music player;
-        sf::RenderWindow window;
-        
+        sf::Text text;
+
+        /**************************************************************
+         * Takes a string representing a csv line from a file and 
+         * splits it into its respective parts in a vector. 
+         *
+         * This member function is adapted from the website
+         * http://ysonggit.github.io/coding/2014/12/16/split-a-string-using-c.html
+         *
+         * Param: &line a reference to the string being split.
+         * Param: delimiter the token used to split this string.
+         * Return: The split line in a vector of strings.
+         *************************************************************/
+        std::vector<std::string> splitString(std::string &line, char delimiter);
+
+        void initEntities(std::string &entitiesFilename);
+        void initInstructions(std::string &instructionsFilename);
+        void initSounds(std::string &soundBufferList);
+        void updateFrame();
     };
   }
 }
