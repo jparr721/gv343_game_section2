47d8be11637dde34bcec620369f86bf649e4b240
Ira Woodring <irawoodring@gmail.com>
2018-10-05 22:08:33 -0400

    Add event polling.

--- Intro.cpp
+++ Intro.cpp
@@ -5,76 +5,78 @@
 #include "SFML/Audio.hpp"
 
 namespace intro {
-    Intro::Intro(sf::RenderWindow &rw) {
-      this->window = &rw;
-    }
+	Intro::Intro(sf::RenderWindow &rw) : window(rw) {
 
-    int Intro::show() {
-        this->window->clear();
-        this->showLogo();
-        return 0;
-    }
+	}
 
-    int Intro::showLogo() {
-        sf::Texture logo;
-        sf::Color defaultColor(255, 255, 255, 0);
+	int Intro::show() {
+		this->window.clear();
+		this->showLogo();
+		return 0;
+	}
 
-        if (!logo.loadFromFile("./images/the_lord.jpg")) {
-            return EXIT_FAILURE;
-        }
+	int Intro::showLogo() {
+		sf::Texture logo;
+		sf::Color defaultColor(255, 255, 255, 0);
 
-        sf::Sprite sprite;
-        sf::Color spriteColor;
-        sprite.setTexture(logo);
-        sprite.scale(2.0f, 2.0f);
-        spriteColor = sprite.getColor();
-        spriteColor.a = 0;
-        sprite.setColor(spriteColor);
+		if (!logo.loadFromFile("./images/the_lord.jpg")) {
+			return EXIT_FAILURE;
+		}
 
-        sf::Font font;
-        if (!font.loadFromFile("fonts/Wonder.ttf")) {
-            return EXIT_FAILURE;
-        }
+		sf::Sprite sprite;
+		sf::Color spriteColor;
+		sprite.setTexture(logo);
+		sprite.scale(2.0f, 2.0f);
+		spriteColor = sprite.getColor();
+		spriteColor.a = 0;
+		sprite.setColor(spriteColor);
 
-        sf::Text title;
-        title.setFont(font);
-        title.setString("IRA PRODUCTIONS PRESENTS");
-        title.setCharacterSize(32);
-        title.setFillColor(defaultColor);
+		sf::Font font;
+		if (!font.loadFromFile("fonts/Wonder.ttf")) {
+			return EXIT_FAILURE;
+		}
 
-        // Position our elements
-        sf::FloatRect txtRect = title.getGlobalBounds();
-        sf::FloatRect picRect = sprite.getGlobalBounds();
-        float txtPosX = (WIDTH - txtRect.width) / 2;
-        float picPoxX = (WIDTH - picRect.width) / 2;
+		sf::Text title;
+		title.setFont(font);
+		title.setString("IRA PRODUCTIONS PRESENTS");
+		title.setCharacterSize(32);
+		title.setFillColor(defaultColor);
 
-        title.setPosition(txtPosX, HEIGHT - 50);
-        sprite.setPosition(picPoxX, 50);
+		// Position our elements
+		sf::FloatRect txtRect = title.getGlobalBounds();
+		sf::FloatRect picRect = sprite.getGlobalBounds();
+		float txtPosX = (WIDTH - txtRect.width) / 2;
+		float picPoxX = (WIDTH - picRect.width) / 2;
 
-        sf::Clock clock;
-        float timeElapsed = 0.0f;
-        int alpha = 0;
-        Animator alphaAnimator;
+		title.setPosition(txtPosX, HEIGHT - 50);
+		sprite.setPosition(picPoxX, 50);
 
-        alphaAnimator.init(0, 255, alpha, 1.0f);
+		sf::Clock clock;
+		float timeElapsed = 0.0f;
+		int alpha = 0;
+		Animator alphaAnimator;
 
-        while (timeElapsed < 5.0f) {
-            this->window->clear();
-            this->window->draw(sprite);
-            this->window->draw(title);
-            this->window->display();
+		alphaAnimator.init(0, 255, alpha, 1.0f);
 
-            alphaAnimator.run(clock);
+		sf::Event event;
+		while (timeElapsed < 10.0f) {
+			window.pollEvent(event);
+			this->window.clear();
+			this->window.draw(sprite);
+			this->window.draw(title);
+			this->window.display();
 
-            defaultColor.a = alpha;
-            spriteColor.a = alpha;
-            title.setFillColor(defaultColor);
-            sprite.setColor(spriteColor);
+			alphaAnimator.run(clock);
 
-            timeElapsed = clock.getElapsedTime().asSeconds();
-        }
+			defaultColor.a = alpha;
+			spriteColor.a = alpha;
+			title.setFillColor(defaultColor);
+			sprite.setColor(spriteColor);
 
-        return 0;
-    }
+			timeElapsed = clock.getElapsedTime().asSeconds();
+		}
+
+		return 0;
+	}
 }
 
