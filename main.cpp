6637372875dbf484c991d5bfd4a7656cc23ec6c1
Justin Johns <johnsju@mail.gvsu.edu>
2018-10-07 11:02:24 -0400

    Updating Branch

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
+#include <iostream>
+#include <Settings.hpp>
 #include "SFML/Graphics.hpp"
+#include "Intro.hpp"
 #include "Game.hpp"
 
 int main(int argc, char** argv){
-	Game g;
-	while(true){
-		g.start();
+
+	// Create the global window instance.
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(WIDTH, HEIGHT+100), "Not on my block.");
+
+	// Initialize our intro, and game screens with the window reference.
+	Game g(window);
+	intro::Intro i(window);
+
+	// Show the intro.
+	i.show();
+
+	while(true) {
+		int started = g.start();
+		if (started  < 0) {
+			std::cerr << "Error! Game failed to start" << std::endl;
+			break;
+		}
 		g.run();
 	}
 }
